7ca0ddcb3a20d9ce1cfeb2d2deaf50a8e2991495
saumensd <saumensutradhar@gmail.com>
2021-08-23 22:56:02 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,25 @@
+//1.let and const
+const hubby = 'Omar Sani';
+let phone = 'iphone 15';
+phone = 'Samsung Galaxy S17';
+
+//2. default string
+//5.spread or three dots(...)
+function maxNumber(array = []){
+    const max = Math.max(...array);
+    return max;
+
+}
+const biggest = maxNumber();
+console.log(biggest);
+
+//3. template string
+const myNotes = `I am mojnu of ${hubby}.I dont have a ${phone}.`;
+console.log(myNotes);
+
+//4. arrow function
+function square(x){
+    return x * x;
+}
+const square = x => x * x;
+console.log(square(9));
